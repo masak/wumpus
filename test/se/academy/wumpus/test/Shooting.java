758ded214f55b442ef5cf30d75527d110a5f150c
Carl Masak <cmasak@gmail.com>
2017-07-17 21:16:55 +0200

    Merge two test files

--- test/se/academy/wumpus/test/Shooting.java
+++ test/se/academy/wumpus/test/Shooting.java
@@ -25,14 +25,35 @@ public class Shooting {
         game.setPlayerLocation(room1);
         game.setWumpusLocation(room2);
 
-
-
         game.acceptCommand("shoot 3");
-
-
 
         Assert.assertTrue(outputLines.size() > 0);
         Assert.assertEquals("You missed the Wumpus!", outputLines.get(outputLines.size() - 1));
         Assert.assertFalse(game.isOver());
+    }
+
+    @Test
+    public void outOfArrows() {
+        List<String> outputLines = new ArrayList<>();
+        Game game = new Game(message -> outputLines.add(message));
+        Map<Integer, Room> rooms = new HashMap<>();
+        Room room1 = new Room(1);
+        Room room2 = new Room(2);
+        rooms.put(1, room1);
+        rooms.put(2, room2);
+        Room.connect(room1, room2);
+        game.setRooms(rooms);
+        game.setPlayerLocation(room1);
+        game.setWumpusLocation(room2);
+
+        game.acceptCommand("shoot 3");
+        game.acceptCommand("shoot 3");
+        game.acceptCommand("shoot 3");
+        game.acceptCommand("shoot 3");
+        game.acceptCommand("shoot 3");
+
+        Assert.assertTrue(outputLines.size() > 0);
+        Assert.assertEquals("Game over, you have no more arrows", outputLines.get(outputLines.size() - 1));
+        Assert.assertTrue(game.isOver());
     }
 }
